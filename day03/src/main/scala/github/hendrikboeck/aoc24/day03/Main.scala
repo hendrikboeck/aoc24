af4e9f700dcc62ac40982523ea0a098c477fd39f
Hendrik Böck <hendrikboeck.dev@protonmail.com>
2024-12-03 18:18:31 +0100

    Implement Part1 functionality and update Readme

--- day03/src/main/scala/github/hendrikboeck/aoc24/day03/Main.scala
+++ day03/src/main/scala/github/hendrikboeck/aoc24/day03/Main.scala
@@ -2,8 +2,8 @@ package github.hendrikboeck.aoc24.day03
 
 object Main extends App {
 
-    Part1("./resources/test.txt")
-    // Part1("./resources/input.txt")
+    // Part1("./resources/test.txt")
+    Part1("./resources/input.txt")
 
     // Part2("./resources/test.txt")
     // Part2("./resources/input.txt")
--- day03/src/main/scala/github/hendrikboeck/aoc24/day03/Part1.scala
+++ day03/src/main/scala/github/hendrikboeck/aoc24/day03/Part1.scala
@@ -8,10 +8,21 @@ object Part1 {
 
         implicit val codec = Codec.UTF8
 
-        val src   = Source.fromFile(inputPath)
-        val lines = src.getLines().map(_.trim).filter(!_.isEmpty)
+        val src = Source.fromFile(inputPath)
+        val text =
+            try src.mkString.trim
+            finally src.close()
 
-        src.close()
+        val mulPattern = """mul\((\d{1,3}),(\d{1,3})\)""".r
+        val result = mulPattern
+            .findAllIn(text)
+            .map {
+                case mulPattern(x, y) => x.toInt * y.toInt
+                case _                => 0 // neutral element in sum
+            }
+            .sum
+
+        println(result)
     }
 
 }
